3c38235a5e0aea31258fc878988bc27ee6e6e364
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-08-07 10:37:13 +1000

    design

--- src/components/VeggiePatch.js
+++ src/components/VeggiePatch.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Patch from './Patch';
 import Plants from './Plants';
 import '../styles/VeggiePatch.css';
+import Form from 'react-bootstrap/Form'
+
+// plant icons
 import leaf from '../icons/leaf.png';
 import corn from '../icons/corn.png';
 import chilli from '../icons/chilli.png';
@@ -12,7 +15,29 @@ import rhubarb from '../icons/rhubarb.png';
 class VeggiePatch extends Component {
   render() {
     return (
-      <div>
+      <div className="veggie-patch-container">
+        <div className="form-container">
+          <h3>My Veggies</h3>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Select Month</Form.Label>
+              <Form.Control as="select">
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </div>
         <div className='all-plants-container'>
           <Plants plant={chilli}/>
           <Plants plant={rhubarb}/>
