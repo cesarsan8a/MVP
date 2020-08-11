b05c573ef96943859adec475507a8484b0cee536
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-08-11 11:22:35 +1000

    Updates

---
+++ precache-manifest.135c51615aa62ff2695d64c3ae3ee7c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989b1919e63d37a8288c198614001f8c",
+    "url": "/cesarsan8a/MVP/index.html"
+  },
+  {
+    "revision": "28fa7cfec90c877beb39",
+    "url": "/cesarsan8a/MVP/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "b402de3a19e4861177f9",
+    "url": "/cesarsan8a/MVP/static/css/main.bcf4141c.chunk.css"
+  },
+  {
+    "revision": "28fa7cfec90c877beb39",
+    "url": "/cesarsan8a/MVP/static/js/2.fb768d32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cesarsan8a/MVP/static/js/2.fb768d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b402de3a19e4861177f9",
+    "url": "/cesarsan8a/MVP/static/js/main.ea4500ce.chunk.js"
+  },
+  {
+    "revision": "61aaeab6e406c16a8990",
+    "url": "/cesarsan8a/MVP/static/js/runtime-main.36be7a0b.js"
+  },
+  {
+    "revision": "58fe641dfbd7d23b7a189770ff9c4ce4",
+    "url": "/cesarsan8a/MVP/static/media/chilli.58fe641d.png"
+  },
+  {
+    "revision": "cbf6aadb5849a893d0b791b498d0f70b",
+    "url": "/cesarsan8a/MVP/static/media/corn.cbf6aadb.png"
+  },
+  {
+    "revision": "b248283007c3ddad5703a7529e9988c5",
+    "url": "/cesarsan8a/MVP/static/media/leaf.b2482830.png"
+  },
+  {
+    "revision": "77aa96709e2529017ecab2f93f6b1fbb",
+    "url": "/cesarsan8a/MVP/static/media/rhubarb.77aa9670.png"
+  },
+  {
+    "revision": "0044b5c2e3eb8606235cfad525a93c14",
+    "url": "/cesarsan8a/MVP/static/media/rural.0044b5c2.svg"
+  }
+]);
